605cf1a98820493f4631cb80b9a33f6d90f4026d
Outman Elbouhali <oelbouha@c2r1p8.1337.ma>
2024-10-30 17:44:52 +0100

    adding get_messages function

--- backend/chat/get_messages.py
+++ backend/chat/get_messages.py
@@ -1,3 +1,5 @@
+
+
 from django.http import JsonResponse
 from rest_framework.response import Response
 from django.contrib.auth.models import User
@@ -7,18 +9,14 @@ from django.db.models import Q
 from chat.models import Message
 
 def get_messages(request):
-	print("request: ", request)
 	user1_id = request.GET.get('user1')
 	user2_id = request.GET.get('user2')
-
-	print("user 1" , user1_id, " user2 ", user2_id)
 
 	if not user1_id or not user2_id:
 		return JsonResponse({"message": "Invalid request: user1 and user2 are required"})
 	try:
 		user1 = User.objects.get(id=user1_id)
 		user2 = User.objects.get(id=user2_id)
-		print("users: ", user1, user2)
 	except User.DoesNotExist:
 		return JsonResponse({"message": "Invalid request: user1 and user2 are required"})
 	
@@ -35,14 +33,14 @@ def get_messages(request):
 		{
 			"sender": message['fields']['sender'],
 			"recipient": message['fields']['recipient'],
-			"type": message['fields']['type'],
+			"tp": message['fields']['type'],
 			"status": message['fields']['status'],
 			"time": message['fields']['time'],
-			"content": message['fields']['content']
+			"cnt": message["fields"]["content"],
+			"msg" : message["pk"]
+
 		}
 		for message in messages_list
+
 	]
 	return JsonResponse(messages_data, safe=False)
-	
-	return JsonResponse({"message": "Messages"})
-#	
